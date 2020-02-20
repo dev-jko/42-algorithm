657ae9ec9dcc3df2207959dc2e07cb41e7aa4a38
nadarm <ccc7497@gmail.com>
2020-02-20 16:16:13 +0900

    linked list

--- linked_list/doubly_linked_list/list.h
+++ linked_list/doubly_linked_list/list.h
@@ -4,3 +4,10 @@ typedef struct	s_list
 	struct s_list	*prev;
 	struct s_list	*next;
 }		t_list;
+
+typedef struct s_linked_list
+{
+	unsigned int	size;
+	t_node		**head;
+	t_node		**tail;
+}		t_linked_list;
