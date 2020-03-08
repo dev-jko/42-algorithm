0935e04a11a4e5745b9ff93d3d90774ece04dbc2
nadarm <ccc7497@gmail.com>
2020-03-08 17:47:22 +0900

    circular linked list implementation

--- linked_list/circular_linked_list/implementation_example/circular_linked_list_implementation.c
+++ linked_list/circular_linked_list/implementation_example/circular_linked_list_implementation.c
@@ -6,16 +6,177 @@
 /*   By: jko <jko@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/07 20:52:54 by jko               #+#    #+#             */
-/*   Updated: 2020/03/08 14:38:50 by jko              ###   ########.fr       */
+/*   Updated: 2020/03/08 17:46:13 by jko              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "list.h"
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+void print_list(t_linked_list *list)
+{
+	t_node	*curr;
+	int	i;
+
+	if (list == 0 || list->size == 0)
+		return ;
+	curr = list->head;
+	i = 0;
+	while (i < list->size)
+	{
+		printf("index %d->data = %s\n", i, curr->data);
+		curr = curr->next;
+		i++;
+	}
+}
+
+void	f(void *addr)
+{
+	char *str = (char *)addr;
+	str[0] = 'a';
+	str[1] = 'b';
+	str[2] = 'c';
+}
 
 int main(int argc, char *argv[])
 {
+	t_linked_list	*list;
+	t_node		*temp;
+	int		test_data_size = 200;
+	char		*test_data[test_data_size];
+	int		test_index[test_data_size];
+	int		index;
+
+	index = 0;
+
+	for (int i = 0; i < test_data_size; ++i) {
+		test_data[i] = malloc(20);
+		sprintf(test_data[i], "str%d", i);
+		test_index[i] = -1 * test_data_size / 2 + i;
+	}
+
+
+	if (!(list = list_init()))
+	{
+		printf("list_init error\n");
+		return (-1);
+	}
+	printf("init list size = %d\n", list_size(list));
+
+	if (!(temp = create_elem(strdup(test_data[0]))))
+	{
+		printf("create_elem error\n");
+		return (-1);
+	}
+	printf("create_elem->data = %s\n", temp->data);
+	printf("temp->prev = %p\n", temp->prev);
+	printf("->next = %p\n", temp->next);
+	free(temp->data);
+	free(temp);
+
+	for (int i = 0; i < 5; ++i) {
+		printf("list_add return value = %d, list_size = %d\n", list_add(list, strdup(test_data[index++]), 0), list_size(list));
+		print_list(list);
+	}
+
+	printf("list_add return value = %d, list_size = %d\n", list_add(list, strdup(test_data[index++]), 5), list_size(list));
+	print_list(list);
+	printf("list_add return value = %d, list_size = %d\n", list_add(list, strdup(test_data[index++]), 7), list_size(list));
+	print_list(list);
+
+	for (int i = 0; i < test_data_size; ++i) {
+		printf("list_get index[%d]->data = %s, list_size = %d\n", test_index[i], ((temp = list_get(list, test_index[i])) ? temp->data : "null"), list_size(list));
+	}
+	print_list(list);
+	printf("list_get index[%d]->data = %s, list_size = %d\n", -1, ((temp = list_get(list, -1)) ? temp->data : "null"), list_size(list));
+	printf("list_get index[%d]->data = %s, list_size = %d\n", -2, ((temp = list_get(list, -2)) ? temp->data : "null"), list_size(list));
+	printf("list_get index[%d]->data = %s, list_size = %d\n", -3, ((temp = list_get(list, -3)) ? temp->data : "null"), list_size(list));
+	printf("list_get index[%d]->data = %s, list_size = %d\n", -4, ((temp = list_get(list, -4)) ? temp->data : "null"), list_size(list));
+	printf("list_get index[%d]->data = %s, list_size = %d\n", -5, ((temp = list_get(list, -5)) ? temp->data : "null"), list_size(list));
+	printf("list_get index[%d]->data = %s, list_size = %d\n", -6, ((temp = list_get(list, -6)) ? temp->data : "null"), list_size(list));
+	printf("list_get index[%d]->data = %s, list_size = %d\n", -7, ((temp = list_get(list, -7)) ? temp->data : "null"), list_size(list));
+
+	for (int i = 0; i < 5; ++i) {
+		list_move_head_to_prev(list);
+		printf("list_move_head_to_prev, list_size = %d\n", list_size(list));
+		print_list(list);
+	}
+
+	for (int i = 0; i < 5; ++i) {
+		list_move_head_to_next(list);
+		printf("list_move_head_to_next, list_size = %d\n", list_size(list));
+		print_list(list);
+	}
 	
+	for (int i = 0; i < 10; ++i) {
+		printf("list_add return value = %d\n", list_add(list, strdup(test_data[index++]), -1));
+		print_list(list);
+	}
+
+	int find_index;
+	for (int i = 0; i < index; ++i) {
+		find_index = list_find(list, test_data[i], &strcmp);
+		if (find_index < 0)
+		{
+			printf("not found\n");
+			continue;
+		}
+		printf("list_find %s -> index = %d, data = %s\n", test_data[i], find_index, list_get(list, find_index)->data);
+	}
+
+	find_index = list_find(list, "abc", &strcmp);
+	if (find_index < 0)
+		printf("not found\n");
+	else
+		printf("list_find %s -> index = %d, data = %s\n", "abc", find_index, list_get(list, find_index)->data);
+
+	printf("list size = %d\n", list_size(list));
+	list_remove(list, test_index[index++], free);
+	printf("list_remove, list_size = %d\n", list_size(list));
+	print_list(list);
+	list_remove(list, test_index[index++], free);
+	printf("list_remove, list_size = %d\n", list_size(list));
+	print_list(list);
+
+	list_clear(list, free);
+	print_list(list);
+
+	for (int i = 0; i < 30; ++i) {
+		printf("list_add return value = %d, list_size = %d\n", list_add(list, strdup(test_data[index]), test_index[index]), list_size(list));
+		print_list(list);
+		index++;
+	}
+
+	//list_clear(list, free);
+	free_list(list, free);
+
+	if (!(list = list_init()))
+	{
+		printf("list_init error\n");
+		return (-1);
+	}
+	printf("init list size = %d\n", list_size(list));
+
+	for (int i = 0; i < 30; ++i) {
+		printf("list_add return value = %d, list_size = %d\n", list_add(list, strdup(test_data[index]), test_index[index]), list_size(list));
+		print_list(list);
+		index++;
+	}
+
+	list_foreach(list, f);	
+	print_list(list);
+	//list_clear(list, free);
+	free_list(list, free);
+
+
+	for (int i = 0; i < test_data_size; ++i) {
+		free(test_data[i]);
+	}
+
+	system("leaks a.out > leaks_result");
+
 	return 0;
 }
 
@@ -68,9 +229,14 @@ void		list_move_head_to_prev(t_linked_list *list)
 
 static int	calculate_index(unsigned int size, int n)
 {
+	int temp; 
+
 	if (size == 0)
 		return (0);
-	return (n % size);
+	if (n >= 0)
+		return (n % size);
+	temp = (size - n) % size * -1;
+	return (temp == 0 ? -size : temp);
 }
 
 static t_node	*move_node(t_node *curr, int direction)
@@ -125,7 +291,7 @@ static int	insert_next(t_linked_list *list, t_node *curr, void *data, int index)
 	new->next = curr->next;
 	curr->next->prev = new;
 	new->prev = curr;
-	curr->prev = new;
+	curr->next = new;
 	return (index != 0 ? index : list->size - 1);
 }
 
@@ -155,7 +321,7 @@ int		list_add(t_linked_list *list, void *data, int n)
 	if ((curr = list_get(list, n)) == 0)
 		return (list_add_last(list, data));
 	if ((n = calculate_index(list->size, n)) < 0)
-		return (insert_next(list, curr, data, n));
+		return (insert_next(list, curr, data, list->size - n - 1));
 	return (insert_prev(list, curr, data, n));
 }
 
@@ -194,22 +360,24 @@ void		list_remove(t_linked_list *list, int n, void (*free_data)(void *))
 
 void		list_clear(t_linked_list *list, void (*free_data)(void *))
 {
-	t_node *curr;
-	t_node *temp;
+	t_node	*curr;
+	t_node	*temp;
+	int	i;
 
-	if (list == 0 || free_data == 0)
+	if (list == 0 || free_data == 0 || list->size == 0)
 		return ;
 	curr = list->head;
-	list->head = 0;
-	list->size = 0;
-	while (curr)
+	i = 0;
+	while (i < list->size)
 	{
 		temp = curr;
 		curr = curr->next;
-		temp->next = 0;
 		free_data(temp->data);
 		free(temp);
+		i++;
 	}
+	list->head = 0;
+	list->size = 0;
 }
 
 void		free_list(t_linked_list *list, void (*free_data)(void *))
