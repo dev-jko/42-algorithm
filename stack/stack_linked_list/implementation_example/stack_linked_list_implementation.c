6f5c5c896c91f7c2282d4f4faa1f8d35d0be0c9a
nadarm <ccc7497@gmail.com>
2020-03-12 02:13:44 +0900

    stack linked list implementation

--- stack/stack_linked_list/implementation_example/stack_linked_list_implementation.c
+++ stack/stack_linked_list/implementation_example/stack_linked_list_implementation.c
@@ -6,12 +6,11 @@
 /*   By: jko <jko@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/12 01:05:04 by jko               #+#    #+#             */
-/*   Updated: 2020/03/12 01:28:02 by jko              ###   ########.fr       */
+/*   Updated: 2020/03/12 01:40:13 by jko              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stack.h"
-
 
 t_stack	*stack_init(void)
 {
@@ -67,17 +66,38 @@ t_node	*stack_pop(t_stack *stack)
 {
 	t_node *node;
 
-	if (stack == 0)
+	if (stack == 0 || stack->size <= 0)
 		return (0);
+	stack->size--;
 	node = stack->top;
-
+	stack->top = node->next;
+	node->next = 0;
 	return (node);
 }
 
 void	stack_clear(t_stack *stack, void (*free_data)(void *))
 {
+	t_node *curr;
+	t_node *temp;
+
+	if (stack == 0 || free_data == 0)
+		return ;
+	curr = stack->top;
+	stack->top = 0;
+	stack->size = 0;
+	while (curr)
+	{
+		temp = curr;
+		curr = curr->next;
+		free_data(temp->data);
+		free(temp);
+	}
 }
 
 void	free_stack(t_stack *stack, void (*free_data)(void *))
 {
+	if (stack == 0 || free_data == 0)
+		return ;
+	stack_clear(stack, free_data);
+	free(stack);
 }
---
+++ stack/stack_linked_list/implementation_example/test_main.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_main.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jko <jko@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/12 00:50:22 by jko               #+#    #+#             */
+/*   Updated: 2020/03/12 02:12:35 by jko              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "stack.h"
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(void)
+{
+	t_node	*temp;
+	t_stack *stack = stack_init();
+	printf("size = %d\n", stack_size(stack));
+	printf("peek->data = %s\n", stack_peek(stack) ? stack_peek(stack)->data : 0);
+	
+	stack_push(stack, strdup("aa"));
+	printf("size = %d\n", stack_size(stack));
+	printf("peek->data = %s\n", stack_peek(stack) ? stack_peek(stack)->data : 0);
+
+	stack_push(stack, strdup("b"));
+	printf("size = %d\n", stack_size(stack));
+	printf("peek->data = %s\n", stack_peek(stack) ? stack_peek(stack)->data : 0);
+
+	stack_push(stack, strdup("ccc"));
+	printf("size = %d\n", stack_size(stack));
+	printf("peek->data = %s\n", stack_peek(stack) ? stack_peek(stack)->data : 0);
+
+	stack_push(stack, strdup("ddddd"));
+	printf("size = %d\n", stack_size(stack));
+	printf("peek->data = %s\n", stack_peek(stack) ? stack_peek(stack)->data : 0);
+
+	stack_push(stack, strdup("ee"));
+	printf("size = %d\n", stack_size(stack));
+	printf("peek->data = %s\n", stack_peek(stack) ? stack_peek(stack)->data : 0);
+
+	stack_push(stack, strdup("f"));
+	printf("size = %d\n", stack_size(stack));
+	printf("peek->data = %s\n", stack_peek(stack) ? stack_peek(stack)->data : 0);
+
+	printf("=============== pop ===============\n");
+	for (int i = 0; i < 9; i++)
+	{
+		temp = stack_pop(stack);
+		printf("pop = %s\n", temp ? temp->data : 0);
+		free(temp ? temp->data : 0);
+		free(temp);
+		printf("size = %d\n", stack_size(stack));
+		printf("peek->data = %s\n", stack_peek(stack) ? stack_peek(stack)->data : 0);
+	}
+
+	stack_clear(stack, free);
+
+	stack_push(stack, strdup("e22"));
+	printf("size = %d\n", stack_size(stack));
+	printf("peek->data = %s\n", stack_peek(stack) ? stack_peek(stack)->data : 0);
+
+	stack_push(stack, strdup("f22"));
+	printf("size = %d\n", stack_size(stack));
+	printf("peek->data = %s\n", stack_peek(stack) ? stack_peek(stack)->data : 0);
+	
+	free_stack(stack, free);
+
+	system("leaks a.out > leaks_result; cat leaks_result | grep leaked");
+	return (0);
+}
