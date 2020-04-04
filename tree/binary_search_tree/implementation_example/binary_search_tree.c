f643d7970e9b27f514018405ded8cd09c9557e8a
nadarm <ccc7497@gmail.com>
2020-04-04 21:14:46 +0900

    binary search tree init

---
+++ tree/binary_search_tree/implementation_example/binary_search_tree.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   binary_search_tree.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jko <jko@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/04 21:13:23 by jko               #+#    #+#             */
+/*   Updated: 2020/04/04 21:14:17 by jko              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "tree.h"
+
+t_tree	*tree_init(int (*cmp)(void *, void *))
+{
+
+}
+
+t_node	*create_elem(void *data)
+{
+
+}
+
+int	tree_insert(t_tree *tree, void *data)
+{
+
+}
+
+t_node	*tree_find(t_tree *tree, void *data_ref)
+{
+
+}
+
+int	tree_delete(t_tree *tree, void *data_ref, void (*free_data)(void *))
+{
+
+}
+
+void	free_tree(t_tree *tree, void (*free_data)(void *))
+{
+
+}
---
+++ tree/binary_search_tree/implementation_example/tree.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tree.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jko <jko@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/04 21:11:12 by jko               #+#    #+#             */
+/*   Updated: 2020/04/04 21:13:05 by jko              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TREE_H
+# define TREE_H
+
+# include <stdlib.h>
+
+typedef struct	s_node
+{
+	void		*data;
+	struct s_node	*left;
+	struct s_node	*right;
+}		t_node;
+
+typedef struct	s_binary_search_tree
+{
+	t_node	*root;
+	int	(*cmp)(void *, void *);
+}		t_tree;
+
+t_tree	*tree_init(int (*cmp)(void *, void *));
+t_node	*create_elem(void *data);
+int	tree_insert(t_tree *tree, void *data);
+t_node	*tree_find(t_tree *tree, void *data_ref);
+int	tree_delete(t_tree *tree, void *data_ref, void (*free_data)(void *));
+void	free_tree(t_tree *tree, void (*free_data)(void *));
+
+#endif
