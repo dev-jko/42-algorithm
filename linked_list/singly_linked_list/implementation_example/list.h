6cf7cfbb9a038c4190d7eaa851afd6b7f8d20906
nadarm <ccc7497@gmail.com>
2020-04-05 13:35:36 +0900

    singly linked list

---
+++ linked_list/singly_linked_list/implementation_example/list.h
@@ -0,0 +1,5 @@
+typedef struct	s_node
+{
+	int		data;
+	struct s_node	*next;
+}		t_node;
--- linked_list/singly_linked_list/implementation_example/singly_linked_list_implementation.c
+++ linked_list/singly_linked_list/implementation_example/singly_linked_list_implementation.c
@@ -149,17 +149,18 @@ int list_add(t_node **begin_list, int data, int n)
 
 int main(void)
 {
-	t_node **begin_list = (t_node **)malloc(sizeof(t_node *));
-	printf("size = %d\n", list_size(*begin_list));
-	list_add1(begin_list, 0);
-	printf("size = %d\n", list_size(*begin_list));
-	list_add1(begin_list, 1);
-	printf("size = %d\n", list_size(*begin_list));
-	list_add1(begin_list, 2);
-	printf("size = %d\n", list_size(*begin_list));
-	list_add1(begin_list, 3);
-	printf("size = %d\n", list_size(*begin_list));
-	t_node *curr = *begin_list;
+
+	t_node *begin_list = 0;
+	printf("size = %d\n", list_size(begin_list));
+	list_add1(&begin_list, 0);
+	printf("size = %d\n", list_size(begin_list));
+	list_add1(&begin_list, 1);
+	printf("size = %d\n", list_size(begin_list));
+	list_add1(&begin_list, 2);
+	printf("size = %d\n", list_size(begin_list));
+	list_add1(&begin_list, 3);
+	printf("size = %d\n", list_size(begin_list));
+	t_node *curr = begin_list;
 	for (; curr; curr = curr->next)
 	{
 		printf("%d\n", curr->data);
@@ -167,28 +168,27 @@ int main(void)
 
 	for (int i = -2; i < 5; i++)
 	{
-		printf("%p\n", list_get(*begin_list, i));
+		printf("%p\n", list_get(begin_list, i));
 	}
 
 	for (int i = -2; i < 6; i++)
 	{
-		printf("find = %d\n", list_find(*begin_list, i));
+		printf("find = %d\n", list_find(begin_list, i));
 	}
 
-	printf("remove = %d\n", list_remove(begin_list, 3));
-	curr = *begin_list;
+	printf("remove = %d\n", list_remove(&begin_list, 3));
+	curr = begin_list;
 	for (; curr; curr = curr->next)
 	{
 		printf("%d\n", curr->data);
 	}
 
-	printf("add -> %d\n", list_add(begin_list, 10, 5));
-	curr = *begin_list;
+	printf("add -> %d\n", list_add(&begin_list, 10, 5));
+	curr = begin_list;
 	for (; curr; curr = curr->next)
 	{
 		printf("%d\n", curr->data);
 	}
-
 
 	return (0);
 }
