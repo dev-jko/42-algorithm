21b2b28902140687b82d51ce926a03975fe16520
nadarm <ccc7497@gmail.com>
2020-04-11 00:07:46 +0900

    tree heap / split function

--- tree/heap/implementation_example/heap.c
+++ tree/heap/implementation_example/heap.c
@@ -6,13 +6,13 @@
 /*   By: jko <jko@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/09 15:49:29 by jko               #+#    #+#             */
-/*   Updated: 2020/04/10 23:39:05 by jko              ###   ########.fr       */
+/*   Updated: 2020/04/11 00:03:44 by jko              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "heap.h"
 
-t_heap	*heap_init(unsigned int max_size, int (*cmp)(void *, void *))
+t_heap		*heap_init(unsigned int max_size, int (*cmp)(void *, void *))
 {
 		t_heap	*heap;
 
@@ -31,7 +31,7 @@ t_heap	*heap_init(unsigned int max_size, int (*cmp)(void *, void *))
 		return (heap);
 }
 
-int		heap_push(t_heap *heap, void *data)
+int			heap_push(t_heap *heap, void *data)
 {
 		unsigned int	i;
 		void			*temp;
@@ -51,34 +51,30 @@ int		heap_push(t_heap *heap, void *data)
 		return (1);
 }
 
-void	*heap_peek(t_heap *heap)
+void		*heap_peek(t_heap *heap)
 {
 		if (!heap || heap->size < 1)
 				return (0);
 		return (heap->data[1]);
 }
 
-void	*heap_pop(t_heap *heap)
+static void	fill_node(t_heap *heap)
 {
-		void			*result;
-		void			*temp;
 		unsigned int	i;
 		unsigned int	j;
 		unsigned int	k;
+		void			*temp;
 
-		if (!heap || heap->size < 1)
-				return (0);
-		result = heap->data[1];
-		heap->data[1] = heap->data[heap->size--];
 		j = 1;
 		while ((i = j))
 		{
-				j = i * 2 <= heap->size && heap->cmp(heap->data[i], heap->data[i * 2]) > 0;
+				j = i * 2 <= heap->size &&
+						heap->cmp(heap->data[i], heap->data[i * 2]) > 0;
 				k = i * 2 + 1 <= heap->size
 						&& heap->cmp(heap->data[i], heap->data[i * 2 + 1]) > 0;
 				if (j && k)
-						j = heap->data[i * 2] < heap->data[i * 2 + 1]
-								? i * 2 : i * 2 + 1;
+						j = heap->cmp(heap->data[i * 2], heap->data[i * 2 + 1])
+								< 0 ? i * 2 : i * 2 + 1;
 				else if (j)
 						j = i * 2;
 				else if (k)
@@ -89,10 +85,21 @@ void	*heap_pop(t_heap *heap)
 				heap->data[i] = heap->data[j];
 				heap->data[j] = temp;
 		}
+}
+
+void		*heap_pop(t_heap *heap)
+{
+		void	*result;
+
+		if (!heap || heap->size < 1)
+				return (0);
+		result = heap->data[1];
+		heap->data[1] = heap->data[heap->size--];
+		fill_node(heap);
 		return (result);
 }
 
-void	free_heap(t_heap *heap, void (*free_data)(void *))
+void		free_heap(t_heap *heap, void (*free_data)(void *))
 {
 		if (!heap || !free_data)
 				return ;
