47f7d0f85ac93abd45fd1b4c239ec4174a943362
nadarm <ccc7497@gmail.com>
2020-04-16 01:12:17 +0900

    sort / quick

--- sort/quick/implementation/quick.c
+++ sort/quick/implementation/quick.c
@@ -6,7 +6,7 @@
 /*   By: jko <jko@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/14 17:55:48 by jko               #+#    #+#             */
-/*   Updated: 2020/04/16 01:07:04 by jko              ###   ########.fr       */
+/*   Updated: 2020/04/16 01:11:34 by jko              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,24 @@ static void	quick(void **items, int start, int end, int (*cmp)(void *, void *))
 {
 	int		i;
 	int		j;
-	int		pivot;
 	void	*temp;
 
 	if (start >= end)
 		return ;
-	pivot = start;
 	i = start;
 	j = end;
 	while (i < j)
 	{
-		while (cmp(items[pivot], items[j]) < 0)
+		while (cmp(items[start], items[j]) < 0)
 			j--;
-		while (i < j && cmp(items[pivot], items[i]) >= 0)
+		while (i < j && cmp(items[start], items[i]) >= 0)
 			i++;
 		temp = items[i];
 		items[i] = items[j];
 		items[j] = temp;
 	}
-	temp = items[pivot];
-	items[pivot] = items[i];
+	temp = items[start];
+	items[start] = items[i];
 	items[i] = temp;
 	quick(items, start, i - 1, cmp);
 	quick(items, i + 1, end, cmp);
